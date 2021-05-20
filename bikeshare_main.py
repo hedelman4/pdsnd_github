6e2ffb3270d16f6be312dea47da4e962e7922226
Harry <hte488@gmail.com>
2021-05-20 11:13:26 -0400

    Limit inputtable months to reflect source data

--- bikeshare_main.py
+++ bikeshare_main.py
@@ -27,7 +27,7 @@ def get_filters():
 
     # get user input for month (all, january, february, ... , june)
     month = input('Enter Month:').lower()
-    while month not in ['all','january', 'february','march','april','may','june','july','august','september','october','november','december']:
+    while month not in ['all','january', 'february','march','april','may','june']:
         month = bsi.month_input()
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
